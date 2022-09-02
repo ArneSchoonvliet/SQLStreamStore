c67dcd7ea220952beb29398382428ca26718fe2b
Arne Schoonvliet <arne@intigriti.com>
2022-09-02 14:32:26 +0200

    Clean not used db implementations

--- src/SqlStreamStore.SchemaCreationScriptTool/Program.cs
+++ src/SqlStreamStore.SchemaCreationScriptTool/Program.cs
@@ -2,10 +2,8 @@
 {
     using System;
     using System.ComponentModel.DataAnnotations;
-    using System.Data.SqlClient;
     using System.IO;
     using McMaster.Extensions.CommandLineUtils;
-    using MySqlConnector;
     using Npgsql;
     using Serilog;
 
@@ -50,79 +48,6 @@
             var exitCode = 0; 
             switch(SQLDialect.ToLowerInvariant())
             {
-                case "mssqlv2":
-#pragma warning disable 618
-                    var mssqlV2Settings = new MsSqlStreamStoreSettings(new SqlConnectionStringBuilder
-                    {
-                        DataSource = "tcp:0.0.0.0,1433"
-                    }.ConnectionString);
-                    if(!string.IsNullOrEmpty(Schema))
-                    {
-                        mssqlV2Settings.Schema = Schema;
-                    }
-
-                    if(CreateSchema)
-                    {
-                        var script = string.Join(
-                            Environment.NewLine,
-                            $@"IF NOT EXISTS (
-SELECT  schema_name
-FROM    information_schema.schemata
-WHERE   schema_name = '{Schema}' ) 
-
-BEGIN
-EXEC sp_executesql N'CREATE SCHEMA {Schema}'
-END",
-                            new MsSqlStreamStore(mssqlV2Settings).GetSchemaCreationScript());
-                        File.WriteAllText(Output, script);
-                    }
-                    else
-                    {
-                        File.WriteAllText(Output, new MsSqlStreamStore(mssqlV2Settings).GetSchemaCreationScript());    
-                    }
-#pragma warning restore 618
-                    break;
-                case "mssqlv3": 
-                    var mssqlV3Settings = new MsSqlStreamStoreV3Settings(new SqlConnectionStringBuilder
-                    {
-                        DataSource = "tcp:0.0.0.0,1433"
-                    }.ConnectionString);
-                    if(!string.IsNullOrEmpty(Schema))
-                    {
-                        mssqlV3Settings.Schema = Schema;
-                    }
-                    if(CreateSchema)
-                    {
-                        var script = string.Join(
-                            Environment.NewLine,
-                            $@"IF NOT EXISTS (
-SELECT  schema_name
-FROM    information_schema.schemata
-WHERE   schema_name = '{Schema}' ) 
-
-BEGIN
-EXEC sp_executesql N'CREATE SCHEMA {Schema}'
-END",
-                            new MsSqlStreamStoreV3(mssqlV3Settings).GetSchemaCreationScript());
-                        File.WriteAllText(Output, script);
-                    }
-                    else
-                    {
-                        File.WriteAllText(Output, new MsSqlStreamStoreV3(mssqlV3Settings).GetSchemaCreationScript());
-                    }
-                    
-                    break;
-                case "mysql": 
-                    var mysqlSettings = new MySqlStreamStoreSettings(new MySqlConnectionStringBuilder
-                    {
-                        Server = "0.0.0.0"
-                    }.ConnectionString);
-                    if(!string.IsNullOrEmpty(Schema))
-                    {
-                        Log.Information("The optional database schema does not apply to the mysql dialect and can be omitted: {Schema}", Schema);
-                    }
-                    File.WriteAllText(Output, new MySqlStreamStore(mysqlSettings).GetSchemaCreationScript());
-                    break;
                 case "postgres": 
                     var postgresSettings = new PostgresStreamStoreSettings(new NpgsqlConnectionStringBuilder
                     {
