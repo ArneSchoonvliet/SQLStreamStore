9e02ab4b318b7dd92152b9474eaaa971ad76c280
Arne Schoonvliet <arne@intigriti.com>
2023-01-06 09:43:38 +0100

    Adjust functions for resync specific

--- src/SqlStreamStore.Postgres/PgSqlScripts/Migration.sql
+++ src/SqlStreamStore.Postgres/PgSqlScripts/Migration.sql
@@ -1,7 +1,7 @@
 COMMENT ON SCHEMA __schema__ IS '{ "version": 3 }';
 
-DROP FUNCTION __schema__.read_all(int4, int8, bool, bool);
-CREATE OR REPLACE FUNCTION __schema__.read_all(
+DROP FUNCTION __schema__.read_all2(int4, int8, bool, bool);
+CREATE OR REPLACE FUNCTION __schema__.read_all2(
   _count    INT,
   _position BIGINT,
   _forwards BOOLEAN,
