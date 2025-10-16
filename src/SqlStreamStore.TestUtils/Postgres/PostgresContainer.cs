aa553761e7c1f4dbc1f8c99b3c40260574f2475a
Brecht Carlier <brecht.carlier@outlook.com>
2025-10-16 09:05:02 +0200

    fix: make loadtests run with more concurrent writers

--- src/SqlStreamStore.TestUtils/Postgres/PostgresContainer.cs
+++ src/SqlStreamStore.TestUtils/Postgres/PostgresContainer.cs
@@ -21,7 +21,8 @@ namespace SqlStreamStore.TestUtils.Postgres
                 Port = Port,
                 Username = "postgres",
                 Host = "127.0.0.1",
-                Pooling = true
+                Pooling = true,
+                MaxPoolSize = 1000
             })
         {
             _containerService = new Builder()
@@ -32,7 +33,7 @@ namespace SqlStreamStore.TestUtils.Postgres
                 .ReuseIfExists()
                 .WithEnvironment("POSTGRES_PASSWORD=password")
                 .ExposePort(Port, Port)
-                .Command("", "-c shared_buffers=128MB")
+                .Command("", "-c shared_buffers=128MB -c max_connections=1000")
                 .Build();
         }
 
